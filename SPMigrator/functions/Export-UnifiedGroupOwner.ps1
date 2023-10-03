08549c6e3ef3bb05d333cbf0c46a5b8df5b71451
Mike Campbell <exactmike@users.noreply.github.com>
2023-10-03 18:08:40 -0400

    fix Export-UnifiedGroupOwner

--- SPMigrator/functions/Export-UnifiedGroupOwner.ps1
+++ SPMigrator/functions/Export-UnifiedGroupOwner.ps1
@@ -6,7 +6,7 @@
     .DESCRIPTION
         Get all Azure Active Directory Unified Groups and export their Drive Info to Excel including DriveName, DriveURL, DriveID, driveType, SiteURL, createdDateTime, lastModifiedDateTime, and quota information
     .EXAMPLE
-        Export-AzureADGroupDrive -OutputFolderPath "C:\Users\UserName\Documents"
+        Export-UnifiedGroupOwner -OutputFolderPath "C:\Users\UserName\Documents"
         All Unified Groups in the connected tenant (via Graph) Drive Info  will be exported to an Excel file in Documents
     #>
 
@@ -25,14 +25,14 @@
     $OutputFilePath = Join-Path -Path $OutputFolderPath -ChildPath $($OutputFileName + '.xlsx')
 
     Write-Information -MessageData 'Getting All Entra Unified Groups'
-    $Groups = Get-OGGroup -UnifiedAll -Property $Property
+    $Groups = Get-OGGroup -UnifiedAll
 
     Write-Information -MessageData 'Filtering Entra Groups for Unified Groups, then getting the Group Owners'
 
     $UnifiedGroupOwners = @($Groups.foreach({
                 $GID = $_.ID
-                Get-MgGroupOwner -GroupID $_.ID |
-                Select-Object -Property @{n='OwnerID';e={$_.ID}},@{n='GroupID';e={$GID}} -ExcludeProperty *
+                Get-MgGroupOwner -GroupID $GID |
+                Select-Object -Property @{n='OwnerID';e={$_.ID}},@{n='GroupID';e={$GID}}
             }))
 
     $UnifiedGroupOwners | Export-Excel -Path $OutputFilePath -WorksheetName UnifiedGroupOwners -TableName UnifiedGroupOwners -TableStyle Medium4
