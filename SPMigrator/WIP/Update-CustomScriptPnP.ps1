1b4321bc8a19f1b80a776d2118e7d7439e3ae2a5
Mike Campbell <exactmike@users.noreply.github.com>
2025-12-16 09:05:41 -0500

    one more update to add

---
+++ SPMigrator/WIP/Update-CustomScriptPnP.ps1
@@ -0,0 +1,18 @@
+﻿param(
+	[string[]]$URL
+	,
+	$SharePointAdminSite
+
+)
+
+$connection = Connect-PnPOnline -Url $SharePointAdminSite -Interactive -ReturnConnection
+
+Foreach ($row in $URL)
+{
+	$siteUrl = $row.SiteUrl
+
+	#change switch to $true to disable custom script
+	Set-PnPTenantSite -Identity $siteUrl -DenyAddAndCustomizePages:$false
+	Write-Host "Updated $($siteUrl)"
+}
+Disconnect-PnPOnline
